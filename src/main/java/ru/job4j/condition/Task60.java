90525835e24f67e1c116b11286d1465ff327f0c8
devvk <info@sekretov.net>
2023-10-30 18:50:40 +0100

    60. Определить соотношение трех чисел

---
+++ src/main/java/ru/job4j/condition/Task60.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+public class Task60 {
+    public static void numRelation(int a, int b, int c) {
+        if (a == b && a == c) {
+            System.out.println("Все равны");
+        } else if (a != b && b != c && a != c) {
+            System.out.println("Равных нет");
+        } else if (a == c) {
+            if (b > a) {
+                System.out.println("Первое и третье равны, второе больше");
+            } else {
+                System.out.println("Первое и третье равны, второе меньше");
+            }
+        } else if (a == b) {
+            if (c > a) {
+                System.out.println("Первое и второе равны, третье больше");
+            } else {
+                System.out.println("Первое и второе равны, третье меньше");
+            }
+        } else {
+            if (b > a) {
+                System.out.println("Второе и третье равны, первое меньше");
+            } else {
+                System.out.println("Второе и третье равны, первое больше");
+            }
+        }
+    }
+}
+
