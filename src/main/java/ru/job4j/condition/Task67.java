af06a79bdfdfe293ad5c8c1c9470237622af7b78
devvk <info@sekretov.net>
2023-10-30 19:35:46 +0100

    67. Являются ли три числа углами одного и того же треугольника

---
+++ src/main/java/ru/job4j/condition/Task67.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Task67 {
+    public static void isTriangle(int a, int b, int c) {
+        if (a + b + c == 180) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+
+}
