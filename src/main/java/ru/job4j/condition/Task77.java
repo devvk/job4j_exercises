60a7c7dc130d87b3b7b20651cb2a7da515351f6b
devvk <info@sekretov.net>
2024-02-22 13:42:19 +0100

    77. Количество разных цифр в трехзначном числе

---
+++ src/main/java/ru/job4j/condition/Task77.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Task77 {
+    public static void diffNumAmount(int num) {
+        String str = Integer.toString(num);
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(str.length() - count);
+    }
+}
