98f04de259b0e26e46ff88bbca6b6bd3379b25de
devvk <info@sekretov.net>
2024-02-28 12:42:32 +0100

    36. Вывести двузначные числа, произведение которых является двузначным числом

---
+++ src/main/java/ru/job4j/loop/Task36.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import java.util.StringJoiner;
+
+public class Task36 {
+    public static void loop() {
+        StringJoiner sj = new StringJoiner(" ");
+        for (int i = 10; i < 30; i++) {
+            if ((i / 10) * (i % 10) >= 10) {
+                sj.add(String.valueOf(i));
+            }
+        }
+        System.out.println(sj);
+    }
+}
