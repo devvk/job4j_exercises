399e26a937cb1aa4a1c27f01b5f89db79a795faf
devvk <info@sekretov.net>
2024-02-29 23:41:09 +0100

    61. Вывести трехзначные числа с определенным условием

--- src/main/java/ru/job4j/loop/Task61.java
+++ src/main/java/ru/job4j/loop/Task61.java
@@ -1,4 +1,16 @@
 package ru.job4j.loop;
 
+import java.util.StringJoiner;
+
 public class Task61 {
+    public static void loop() {
+        StringJoiner sj = new StringJoiner(" ");
+        for (int i = 100; i < 500; i++) {
+            if (Math.pow((double) i / 100, 2) + Math.pow(i % 10, 2)
+                    == Math.pow((double) (i % 100) / 10, 2)) {
+                sj.add(String.valueOf(i));
+            }
+        }
+        System.out.println(sj);
+    }
 }
