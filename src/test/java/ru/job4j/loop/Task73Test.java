1cbf9557bdfa324c9db3f5b2433b13ca27e64b58
devvk <info@sekretov.net>
2024-03-04 16:28:10 +0100

    73. Равна ли сумма делителей числа этому числу

---
+++ src/test/java/ru/job4j/loop/Task73Test.java
@@ -0,0 +1,48 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task73Test {
+    private final String ln = System.lineSeparator();
+
+    @Test
+    public void when28ThenYes() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task73.loop(28);
+        String expected = "Да" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void when6ThenYes() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task73.loop(6);
+        String expected = "Да" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void when5ThenNo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task73.loop(5);
+        String expected = "Нет" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void when10ThenNo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task73.loop(10);
+        String expected = "Нет" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+}
