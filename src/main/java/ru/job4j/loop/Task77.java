746bc6ce3a16e630fb552d2b7819e8a00862ee9c
devvk <info@sekretov.net>
2024-03-04 16:58:57 +0100

    77. Вывести сумму общих делителей двух чисел

---
+++ src/main/java/ru/job4j/loop/Task77.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Task77 {
+    public static void loop(int num1, int num2) {
+        int sum = 0;
+        for (int i = 1; i <= Math.min(num1, num2); i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
