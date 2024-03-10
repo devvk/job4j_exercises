84c1ec7e2ad5e639c0347baf89be12b673d5b4d6
devvk <info@sekretov.net>
2024-03-10 23:20:50 +0100

    8. Вывести делители числа

---
+++ src/main/java/ru/job4j/loop/Task8.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Task8 {
+    public static void loop(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/loop/Task8Test.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task8Test {
+    private final String ln = System.lineSeparator();
+
+    @Test
+    public void when1() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task8.loop(1);
+        String expected = "1" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void when2() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task8.loop(2);
+        String expected = "1" + ln + "2" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void when6() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task8.loop(6);
+        String expected = "1" + ln + "2" + ln + "3" + ln + "6" + ln;
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+}
