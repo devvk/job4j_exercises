51515d00b365a99aca119eef2d70a494be5e20d4
devvk <info@sekretov.net>
2024-03-10 22:53:48 +0100

    3. Вывести на консоль числа от N до 0

---
+++ src/test/java/ru/job4j/loop/Task3Test.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task3Test {
+
+    @Test
+    public void out3() {
+        var out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task3.loop(3);
+        var expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("2").add("1").add("0").toString();
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void out2() {
+        var out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task3.loop(2);
+        var expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("1").add("0").toString();
+        assertThat(out.toString()).isEqualTo(expected);
+    }
+}
