85a09e071c514b727e8fc9abc903d8235a4842c5
devvk <info@sekretov.net>
2024-03-15 18:45:40 +0100

    32. Четное или нечетное количество отрицательных значений

--- src/test/java/ru/job4j/array/Task32Test.java
+++ src/test/java/ru/job4j/array/Task32Test.java
@@ -8,14 +8,14 @@ public class Task32Test {
     @Test
     public void checkTrue() {
         int[] data = {-5, -4, -3, -2, -1, 1, 2, 3, 4, 5};
-        boolean expected = Task32.check(data);
-        assertThat(true).isEqualTo(expected);
+        boolean result = Task32.check(data);
+        assertThat(result).isEqualTo(true);
     }
 
     @Test
     public void checkFalse() {
         int[] data = {-4, -3, -2, -1, 1, 2, 3, 4};
-        boolean expected = Task32.check(data);
-        assertThat(false).isEqualTo(expected);
+        boolean result = Task32.check(data);
+        assertThat(result).isEqualTo(false);
     }
 }
