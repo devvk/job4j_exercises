60d520b35b2b3516ec0b8ee00c4e38f9d4a51ffd
devvk <info@sekretov.net>
2024-03-16 14:51:30 +0100

    VisibilityModifier

--- src/test/java/ru/job4j/array/Task79Test.java
+++ src/test/java/ru/job4j/array/Task79Test.java
@@ -8,7 +8,7 @@ import java.io.PrintStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task79Test {
-    public String ln = System.lineSeparator();
+    private final String ln = System.lineSeparator();
 
     @Test
     public void when554331542Then5And4And3() {
