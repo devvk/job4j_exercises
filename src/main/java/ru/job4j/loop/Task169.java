0ad03a94d5fa79ee1294dee04552ca67ee6b2b06
devvk <info@sekretov.net>
2024-03-16 18:08:57 +0100

    169. Вывод песочных часов из "звёздочек"

--- src/main/java/ru/job4j/loop/Task169.java
+++ src/main/java/ru/job4j/loop/Task169.java
@@ -1,17 +1,24 @@
 package ru.job4j.loop;
 
+import java.util.Scanner;
+
 public class Task169 {
     public static void main(String[] args) {
-        int columnCount = 5;
+        Scanner in = new Scanner(System.in);
+        int columnCount = in.nextInt();
         String star = "*";
         String space = " ";
-
+        int starCount = columnCount;
+        int spaceCount = 0;
         for (int i = 0; i < columnCount; i++) {
-            for (int j = 0; j < columnCount - i; j++) {
-                System.out.print(star);
-                System.out.println(space);
+            System.out.println(space.repeat(spaceCount) + star.repeat(starCount));
+            if (i < columnCount / 2) {
+                spaceCount++;
+                starCount -= 2;
+            } else {
+                spaceCount--;
+                starCount += 2;
             }
-            System.out.println();
         }
 
     }
