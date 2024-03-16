023da37a2647ca522928d40095f95df16df0f7da
devvk <info@sekretov.net>
2024-03-16 14:50:19 +0100

    LineLength

--- src/test/java/ru/job4j/array/Task26Test.java
+++ src/test/java/ru/job4j/array/Task26Test.java
@@ -7,7 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Task26Test {
     @Test
     public void test() {
-        assertThat("Иванов И.И.").isEqualTo(Task26.convert(new String[]{"Иванов", "Иван", "Иваныч"}));
-        assertThat("Григорьев И.Н.").isEqualTo(Task26.convert(new String[]{"Григорьев", "Игорь", "Николаевич"}));
+        String result1 = Task26.convert(new String[]{"Иванов", "Иван", "Иваныч"});
+        assertThat("Иванов И.И.").isEqualTo(result1);
+        String result2 = Task26.convert(new String[]{"Григорьев", "Игорь", "Николаевич"});
+        assertThat("Григорьев И.Н.").isEqualTo(result2);
     }
 }
