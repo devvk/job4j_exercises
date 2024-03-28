475c1ddde83cc591fc3d2b227cd93418d6ce7590
devvk <info@sekretov.net>
2024-03-28 16:36:46 +0100

    15. Разрушение бинарного кода

---
+++ src/test/java/ru/job4j/array/DestroySequenceTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DestroySequenceTest {
+    @Test
+    public void test() {
+        assertThat("0000011111".toCharArray()).isEqualTo(DestroySequence.destroy("1111100000".toCharArray()));
+        assertThat("00000011111".toCharArray()).isEqualTo(DestroySequence.destroy("11111000000".toCharArray()));
+        assertThat("0000001011111".toCharArray()).isEqualTo(DestroySequence.destroy("1111101000000".toCharArray()));
+    }
+}
