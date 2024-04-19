e71c1f05fffe1244f47362590c03fd34452535c8
devvk <info@sekretov.net>
2024-04-19 12:08:16 +0200

    Объявить локальный класс

---
+++ src/main/java/ru/job4j/obj/Task14.java
@@ -0,0 +1,15 @@
+package ru.job4j.obj;
+
+public class Task14 {
+
+    public void test() {
+        class Nested {
+            public void print() {
+            }
+        }
+
+        Nested nested = new Nested();
+        nested.print();
+    }
+
+}
