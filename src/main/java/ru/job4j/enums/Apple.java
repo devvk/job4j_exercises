1606bb4a5796c8decb56834933e145fd1f968a33
devvk <info@sekretov.net>
2024-05-07 16:44:39 +0200

    Enum

---
+++ src/main/java/ru/job4j/enums/Apple.java
@@ -0,0 +1,21 @@
+package ru.job4j.enums;
+
+enum Apple {
+    Jonathan(10), GoldenDel(9), RedDel(8), Winesap(15), Cortland(8);
+
+    private final int price;
+
+    Apple(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static void main(String[] args) {
+        for (Apple a : Apple.values()) {
+            System.out.println(a + ": " + a.getPrice());
+        }
+    }
+}
