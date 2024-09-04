f1f92d10392ca521dfe65b76237620f9642ae222
devvk <info@sekretov.net>
2024-09-04 17:41:05 +0200

    1.1. Получение компаратора. Метод naturalOrder()

---
+++ src/test/java/ru/job4j/comparator/NaturalOrderTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.comparator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NaturalOrderTest {
+
+    @Test
+    public void test() {
+        assertEquals(Comparator.naturalOrder(), NaturalOrder.naturalOrder());
+    }
+
+}
