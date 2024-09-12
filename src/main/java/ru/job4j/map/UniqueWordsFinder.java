7bc868048d4f6c5ee57951a9c4c6a314cf916b2b
devvk <info@sekretov.net>
2024-09-12 17:25:07 +0200

    Поиск уникальных слов в списке

--- src/main/java/ru/job4j/map/UniqueWordsFinder.java
+++ src/main/java/ru/job4j/map/UniqueWordsFinder.java
@@ -8,11 +8,11 @@ import java.util.Map;
 public class UniqueWordsFinder {
 
     public List<String> findUniqueWords(List<String> words) {
-        List<String> uniqueWords = new ArrayList<>();
         Map<String, Integer> wordCount = new LinkedHashMap<>();
         for (String word : words) {
             wordCount.merge(word, 1, Integer::sum);
         }
+        List<String> uniqueWords = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             if (entry.getValue() == 1) {
                 uniqueWords.add(entry.getKey());
