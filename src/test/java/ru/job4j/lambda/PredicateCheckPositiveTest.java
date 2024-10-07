6da151546f92800fe3613e8d26dbfa471609924f
devvk <info@sekretov.net>
2024-10-07 20:26:11 +0200

    1.0. Predicate. Проверить, что число положительное

---
+++ src/test/java/ru/job4j/lambda/PredicateCheckPositiveTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PredicateCheckPositiveTest {
+
+    @Test
+    public void whenPositive() {
+        assertTrue(PredicateCheckPositive.check(1));
+        assertFalse(PredicateCheckPositive.check(-1));
+        assertFalse(PredicateCheckPositive.check(0));
+    }
+}
