a72afd391819addefe0bf1a1b1ef55a5f2161da6
devvk <info@sekretov.net>
2024-10-07 20:31:37 +0200

    1.2. Predicate. Проверить, что число положительное и четное

---
+++ src/test/java/ru/job4j/lambda/PredicateCheckEvenAndPositiveTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PredicateCheckEvenAndPositiveTest {
+
+    @Test
+    public void test() {
+        assertTrue(PredicateCheckEvenAndPositive.check(2));
+        assertFalse(PredicateCheckEvenAndPositive.check(-2));
+        assertFalse(PredicateCheckEvenAndPositive.check(1));
+    }
+}
