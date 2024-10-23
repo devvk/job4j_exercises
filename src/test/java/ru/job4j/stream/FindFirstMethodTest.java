5384aa219d81a0f4ef9fcb923374694aaa3e394d
devvk <info@sekretov.net>
2024-10-23 20:33:41 +0200

    5.6. Терминальные операции. Метод findFirst()

---
+++ src/test/java/ru/job4j/stream/FindFirstMethodTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindFirstMethodTest {
+
+    @Test
+    public void test() {
+        assertEquals(Optional.empty(), FindFirstMethod.first(List.of()));
+        assertEquals(Optional.of(1), FindFirstMethod.first(List.of(1)));
+    }
+}
