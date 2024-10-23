e728e77ddcbf3966a41f10fcad7c385e7dcc467e
devvk <info@sekretov.net>
2024-10-23 16:13:25 +0200

    2.4. Понятие редукции. Метод min(). Поиск кратчайшей строки

---
+++ src/test/java/ru/job4j/stream/MinMethodTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MinMethodTest {
+    @Test
+    public void test() {
+        assertThat(MinMethod.min(List.of("ccc", "bb", "y"))).isEqualTo("y");
+    }
+}
