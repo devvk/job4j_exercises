7089126a50efc230b28a831d0c3d855c77022d93
devvk <info@sekretov.net>
2024-10-25 18:14:06 +0200

    6.4. Промежуточные операции. Метод mapToInt(). Сумма кодов символов

---
+++ src/test/java/ru/job4j/stream/MapToIntMethodTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapToIntMethodTest {
+
+    @Test
+    public void test() {
+        assertEquals(294, MapToIntMethod.sum(List.of('a', 'b', 'c')));
+    }
+}
